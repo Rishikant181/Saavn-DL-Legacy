bf07bcdbc51749d403dfd521f025286b61689d41
Rishikant Sahu <rishikantsahu181@gmail.com>
2021-05-29 20:07:58 +0530

    Clean the starting file and made it into a class

--- saavn-dl.py
+++ saavn-dl.py
@@ -1,62 +1,88 @@
 # This is a downloader for JioSaavn that works by making some http request to JioSaavn
 
-# low  -128kbps-125k bitrate
-# med  -256kbps-250k bitrate
-# high -320kbps-500k bitrate
-
 import os
-import sys
 import meta
 import fetcher
+import argparse
 from moviepy.editor import AudioFileClip
 
-param = None                                                    # To store whether playlist or not
-url = None                                                      # To store song original url
-curSong = None                                                  # To store name of current song
+class Main:
+    # The constructor
+    def __init__(self, args):
+        self.url = args['url']                                             # To store song/playlist url
+        self.quality = args['quality']                                     # To store song quality
+        self.fetch = fetcher.Fetcher()                                       # To store object of fetcher class
 
-try:
-    param = sys.argv[1]
-    url = sys.argv[2]
-# If one param is passed
-except:
-    url = sys.argv[1]
-
-fetch = fetcher.Fetcher()
-
-# If downloading playlist
-if(param == 'p'):
-    playList = fetch.getSongsFromPlaylist(url)
-    for song in playList:
-        # Getting url of each song
-        url = song['perma_url']
-        
-        metaData = fetch.download(url)
+    # Method to download each song
+    def downloadSong(self):
+        metaData = self.fetch.download(self.url)
         curSong = metaData['title']
 
         # Converting raw song to mp3
         audioFile = AudioFileClip('temp.mp4')
-        audioFile.write_audiofile((curSong + '.mp3'), bitrate='500k')
+        audioFile.write_audiofile((curSong + '.mp3'), bitrate=(str(125 * self.quality * 2) + "k"))
 
         # Embedding metaData in mp3
         Meta = meta.Metadata(curSong, metaData)
-        Meta.embbed()
+        Meta.embbed()    
 
         # Deleting raw song and cover art
         os.remove('temp.mp4')
         os.remove('temp.jpg')
-# If single song
-else:
-    metaData = fetch.download(url)
-    curSong = metaData['title']
 
-    # Converting raw song to mp3
-    audioFile = AudioFileClip('temp.mp4')
-    audioFile.write_audiofile((curSong + '.mp3'), bitrate='500k')
+    # Method to download a playlist
+    def downloadPlaylist(self):
+        playList = self.fetch.getSongsFromPlaylist(self.url)
+        
+        # Iterating through playlist
+        for song in playList:
+            # Getting url of each song
+            self.url = song['perma_url']
+        
+            # Downloading each song
+            self.downloadSong()
 
-    # Embedding metaData in mp3
-    Meta = meta.Metadata(curSong, metaData)
-    Meta.embbed()
+    # Method to check if url is single song or playlist
+    def isPlaylist(self):
+        # If single song
+        if(self.url.index(".com/song/") != -1):
+            return False
+        # If playlist
+        else:
+            return True
 
-    # Deleting raw song and cover art
-    os.remove('temp.mp4')
-    os.remove('temp.jpg')
+    # Method to download song using either downloadPlaylist or downloadSong method
+    def download(self):
+        # If playlist
+        if(self.isPlaylist() == True):
+            self.downloadPlaylist()
+        # If single sone
+        else:
+            self.downloadSong()
+
+# Method to parser arguments
+def parseArgs():
+    # Creating argument parser
+    parser = argparse.ArgumentParser()
+
+    # Adding arguments
+    parser.add_argument("-u", "--url", help="The URL of the song or playlist to download", type=str)
+    parser.add_argument("-q", "--quality", help="Set quality of downloaded song, 0 for 128 Kbps, 1 for 256 Kbps, 2 for 320 Kbps", type=int, default=3)
+
+    # Parse args then return
+    return parser.parse_args()
+
+# The main method
+def main():
+    # Get parsed args
+    args = parseArgs()
+    
+    # Creating main class object
+    ob = Main(args.__dict__)
+
+    # Downloading song
+    ob.download()        
+
+# Executing main method
+if(__name__ == '__main__'):
+    main()
